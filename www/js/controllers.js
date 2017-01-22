c492dda21787de6e6b0025f5991d4fa307c30eb1
raghavsanthapur <rsanthapur@visionit.com>
2017-01-22 14:12:03 -0500

    ShoppingCart
    Sample Shopping Cart which has only Apples,Oranges which costs .45$ and
    .65$ respectively. Displays the cost depending on number of items
    customer selected.

---
+++ www/js/controllers.js
@@ -0,0 +1,82 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope,$state) {
+  var val =angular.element(document.getElementById('quantity'))[0];
+
+$scope.total = 0;
+
+$scope.items = [
+    
+    {
+      'name' : 'Apple',
+      'cost' : 45,
+      'count' : 0
+    },
+
+    {
+      'name' : 'Orange',
+      'cost' : 65,
+      'count' : 0
+    }
+
+];
+
+$scope.cart = angular.copy($scope.items);
+
+$scope.getReceipt = function(){
+  $scope.total = 0;  
+    for (var j = $scope.items.length - 1; j >= 0; j--) {
+        $scope.total += ($scope.items[j].count * $scope.items[j].cost);
+    }
+  console.log('total ', $scope.total);
+}
+
+$scope.increaseQuantity =function(item){
+  item.count++ ;
+  $scope.getReceipt();
+}
+$scope.decreaseQuantity =function(item){
+
+  item.count--;
+  $scope.getReceipt();
+
+}
+
+$scope.goToCart =function(){
+
+  console.log($scope.quantity);
+  console.log($scope.quantity1);
+  var nextPageData={
+    'apples':$scope.quantity,
+    'oranges':$scope.quantity1
+  }
+  $state.go('tab.chats',{"data":nextPageData});
+
+}
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
